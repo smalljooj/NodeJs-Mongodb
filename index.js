0b028e568d7fd01df3253866878cb3af2a64dba6
Ismael Marinho <firespin147@gmail.com>
2021-05-03 23:30:28 -0300

    Mongoose e criacao de categorias

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 	const app = express()
 	const admin = require(__dirname+'/routes/admin')
 	const path = require('path')
-	//const mongoose = require('mongoose')
+	const mongoose = require('mongoose')
 //config
 	//body-parser
 		app.use(bodyParser.urlencoded({extended : true}))
@@ -14,7 +14,12 @@
 		app.engine('handlebars', handlebars({defaultlayout : 'main'}))
 		app.set('view engine', 'handlebars')
 	//mongoose
-		//
+		mongoose.Promise = global.Promise;
+		mongoose.connect("mongodb://localhost/blogapp").then(()=>{
+			console.log("Conectado ao mongo")
+		}).catch((err)=>{
+			console.log(err+"Erro ao se conectar")
+		});
 	//public
 		app.use(express.static(path.join(__dirname, "public")))
 //routes
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model("categorias");
+
 router.get('/', (req, res)=>{
 	res.render("admin/index");
 })
@@ -12,8 +16,25 @@ router.get('/post', (req, res)=>{
 
 
 router.get('/categorias', (req, res)=>{
-	res.send("pagina de categorias");
+	res.render("admin/categorias");
 })
+
+router.get('/categorias/add', (req, res)=>{
+	res.render("admin/addcategoria");
+})
+
+router.post('/categorias/nova', (req, res)=>{
+	const novaCategoria = {
+		nome: req.body.nome,
+		slug: req.body.slug
+	}
+	new Categoria(novaCategoria).save().then(()=>{
+		console.log("Categoria salva")
+	}).catch((err)=>{
+		console.log("erro:"+err)
+	})
+})
+
 
 
 
